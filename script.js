ce990361a8bb923917e3c93cdf806328d5e44541
Srinivas K <142530260+Sriz24@users.noreply.github.com>
2024-02-12 23:57:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function addTodo(){
+    var todoInput = document.getElementById("todo-input");
+    var todoText = todoInput.value.trim();
+    if (todoText !== "") {
+        var li= document.createElement("li");
+        li.textContent = todoText;
+
+        var deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.classList.add("delete-btn");
+        deleteButton.onclick =function()  {
+            li.remove();
+        };
+        li.appendChild(deleteButton);
+
+      document.getElementById("todo-list").append(li);
+
+    }
+}
